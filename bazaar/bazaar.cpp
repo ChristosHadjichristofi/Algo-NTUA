0a9c950d86b6b4493d4e9a506d779e2458ec5e43
Christos Hadjichristofi <40044042+BeenCoding@users.noreply.github.com>
2020-12-19 16:59:12 +0200

    complete bazaar.cpp

--- bazaar/bazaar.cpp
+++ bazaar/bazaar.cpp
@@ -1,21 +1,19 @@
 #include <iostream>
 #include <bits/stdc++.h>
-#include <fstream>
 using namespace std;
 
-#define cin infile
-#define INTEGER_MAX 1000000001
-#define MAX_M 1501
+#define MAX_INT 999999999
+#define MAX_N 5001
 
 vector <pair<int, int> > A1, A2, A3, B1, B2, B3, C1, C2, C3;
-int A[MAX_M] = {0}, B[MAX_M] = {0}, C[MAX_M] = {0}, Set1[MAX_M] = {0}, Set2[MAX_M] = {0}, Set3[MAX_M] = {0};
+int A[MAX_N] = {0}, B[MAX_N] = {0}, C[MAX_N] = {0}, Set1[MAX_N] = {0}, Set2[MAX_N] = {0}, Set3[MAX_N] = {0};
 int N, M;
 
 void knapsack(int *dp, vector <pair<int, int> > costs){
 
     int totalQuantity = 0;
 
-    for (int i = 1; i <= M; i++) dp[i] = INTEGER_MAX;
+    for (int i = 1; i <= N; i++) dp[i] = MAX_INT;
 
     for (int i = 0; i < costs.size(); i++){
         int currQuantity = costs[i].first, currPrice = costs[i].second;
@@ -27,15 +25,13 @@ void knapsack(int *dp, vector <pair<int, int> > costs){
 }
 
 void completeSet(int Set[]){ 
-    for (int i = 0; i <= M; i++)
-        (A[i] == INTEGER_MAX || B[i] == INTEGER_MAX || C[i] == INTEGER_MAX) ? Set[i] = INTEGER_MAX : Set[i] = A[i] + B[i] + C[i];
+    for (int i = 0; i <= N; i++)
+        (A[i] == MAX_INT || B[i] == MAX_INT || C[i] == MAX_INT) ? Set[i] = MAX_INT : Set[i] = A[i] + B[i] + C[i];
 }
 
 int main(){
 
-    ifstream infile("lab02/bazaar/input0.txt");
-
-    int quantity, price, minPrice = INTEGER_MAX;
+    int quantity, price, minPrice = MAX_INT;
     string itemTrader;
     cin >> N >> M;
 
@@ -57,16 +53,16 @@ int main(){
             else C3.push_back(make_pair(quantity, price)); 
         }
     }
-
+    
     knapsack(A, A1); knapsack(B, B1); knapsack(C, C1); completeSet(Set1);
     knapsack(A, A2); knapsack(B, B2); knapsack(C, C2); completeSet(Set2);
     knapsack(A, A3); knapsack(B, B3); knapsack(C, C3); completeSet(Set3);
 
-    for (int i = 0; i <= M; i++)
-        for (int j = M - i; j >= 0; j--){
-            if (Set1[i] == INTEGER_MAX || Set2[i] == INTEGER_MAX || Set3[i] == INTEGER_MAX) continue;
-            if (minPrice > Set1[i] + Set2[j] + Set3[M - j - i]) minPrice = Set1[i] + Set2[j] + Set3[M - j - i];
+    for (int i = 0; i <= N; i++)
+        for (int j = N - i; j >= 0; j--){
+            if (Set1[i] == MAX_INT || Set2[j] == MAX_INT || Set3[N - j - i] == MAX_INT) continue;
+            if (minPrice > Set1[i] + Set2[j] + Set3[N - j - i]) minPrice = Set1[i] + Set2[j] + Set3[N - j - i];
         }
 
-    (minPrice == INTEGER_MAX) ? cout << "-1" << endl : cout << minPrice << endl;
+    (minPrice == MAX_INT) ? cout << "-1" << endl : cout << minPrice << endl;
 }
